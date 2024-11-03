b692d3a7ddc322d7ee40ae00ef5981aa48598b75
Irish Tegio <irishbtegio@gmail.com>
2024-11-03 20:00:36 +0800

    signin

--- welcome.php
+++ welcome.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Quilana Review Website | Welcome</title>
-    <link rel="stylesheet" href="assets/css/landingpage.css" />
+    <title>Quilana | Welcome</title>
+    <link rel="stylesheet" href="assets/css/landing_page.css" />
   </head>
   <body>
     <main>
